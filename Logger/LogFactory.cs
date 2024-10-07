e48e0e7920c3811455b80e36311046c49b2a6448
David <david_e_moore@hotmail.com>
2024-10-07 16:46:21 -0700

    Updating LogFactory Initializer

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -2,9 +2,17 @@
 
 public class LogFactory
 {
-    public BaseLogger CreateLogger(string className)
-    {
 
-        return null;
+    private string? FilePath { get; set; }
+
+    public BaseLogger? CreateLogger(string className)
+    {
+        if (string.IsNullOrEmpty(FilePath))
+        {
+            return null; //Testing FilePath to see if its Null.
+        }
+        return new FileLogger(FilePath){
+            ClassName = className
+        };
     }
 }
