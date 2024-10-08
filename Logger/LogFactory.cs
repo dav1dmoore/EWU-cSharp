a668e4b7d1828c24f06fa9b4345ed9e3005168b8
David <david_e_moore@hotmail.com>
2024-10-07 17:28:20 -0700

    Built BaseLoggerMixins, Updated csproj

--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,10 +1,18 @@
-﻿namespace Logger;
+﻿using System.IO;
+using System.Reflection;
+
+namespace Logger;
 
 public class LogFactory
 {
 
     private string? FilePath { get; set; }
 
+    public void ConfigureFileLogger()
+    {
+        string assemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+        FilePath = Path.Combine(assemblyLocation, "file.txt");
+    }
     public BaseLogger? CreateLogger(string className)
     {
         if (string.IsNullOrEmpty(FilePath))
